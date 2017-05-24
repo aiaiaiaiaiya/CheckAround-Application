e9bcbf3d27bbaac947f31f2749efefdad569ee68
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2017-05-24 22:44:17 +0700

    Final commited with Spring API

---
+++ src/main/java/com/example/checkaround/controller/checkinController.java
@@ -0,0 +1,56 @@
+package com.example.checkaround.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.checkaround.model.Checkins;
+import com.example.checkaround.model.Users;
+import com.example.checkaround.repository.UserRepository;
+import com.example.checkaround.service.LocationService;
+
+@RestController
+public class checkinController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private LocationService locationService;
+
+	@RequestMapping(value = "/user",method = RequestMethod.POST)
+	public List<Users> userCheckin(@RequestBody Users user){
+		userRepository.save(user);
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/getuser",method = RequestMethod.GET)
+	public List<Users> getUser(){
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(value = "user/{page}/checkin",method = RequestMethod.POST)
+	public List<Users> replies(@PathVariable("page") int page,@RequestBody Checkins checkin){
+		
+		Users u = userRepository.findOne(page);
+		u.getCheckins().add(checkin);
+		userRepository.save(u);
+		
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/showlocations",method = RequestMethod.GET)
+	public List<Checkins> userCheckin(){
+		return locationService.showListOflocations();
+	}
+	
+	
+}
---
+++ src/main/java/com/example/checkaround/model/Checkins.java
@@ -0,0 +1,73 @@
+package com.example.checkaround.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Checkins {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String locationName;
+	private Double latitude;
+	private Double longitude;
+	private String caption;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date timestamp = new Date();
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+}
